6646be0f6264578d84fc3d69eb6cc956bc2932a2
dakang <trumpjqk@daum.net>
2022-06-14 00:52:14 +0900

    220614

---
+++ problem_1000-9999/1330_두_수_비교하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int	main(void)
+{
+	int	A;
+	int	B;
+
+	cin >> A >> B;
+	if (A > B)
+		cout << '>';
+	else if (A < B)
+		cout << '<';
+	else
+		cout << "==";
+	return (0);
+}
